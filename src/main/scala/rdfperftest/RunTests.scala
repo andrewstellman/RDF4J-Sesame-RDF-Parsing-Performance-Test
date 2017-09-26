0f3c081fe3ae030be3b7b4f8a236f3d4fd6eebd0
Andrew Stellman <astellman@stellman-greene.com>
2017-09-26 14:10:19 -0500

    Added command line parameters

--- src/main/scala/rdfperftest/RunTests.scala
+++ src/main/scala/rdfperftest/RunTests.scala
@@ -7,20 +7,32 @@ object RunTests extends App {
 
   private[this] val logger = getLogger
 
-  val file = File(s"test_results_${System.currentTimeMillis}.txt")
-  file.appendLine("threads,rdf4j_statements_per_ms,sesame_statements_per_ms")
-  logger.info(s"Writing test results to ${file.name}")
+  if (args.length != 4) {
+    System.err.println("usage: RunTests minThreads maxThreads warmupMillis testLengthMillis")
+    System.err.println("sbt usage: sbt \"run minThreads maxThreads warmupMillis testLengthMillis\"")
+  } else {
 
-  val warmupMillis = 10000
-  val testLengthMillis = 30000
+    val minThreads = args(0).toInt
+    val maxThreads = args(1).toInt
+    val warmupMillis = args(2).toInt
+    val testLengthMillis = args(3).toInt
+    
+    logger.info(s"Running with minThreads=$minThreads maxThreads=$maxThreads warmupMillis=$warmupMillis testLengthMillis=$testLengthMillis") 
 
-  for (threads <- 1 to 30) {
-    val rdf4jStatementsPerMs = RDFPerformanceTest.run(RDF4J, threads, warmupMillis, testLengthMillis)
-    val sesameStatementsPerMs = RDFPerformanceTest.run(Sesame, threads, warmupMillis, testLengthMillis)
+    val file = File(s"test_results_${System.currentTimeMillis}.txt")
+    file.appendLine("threads,rdf4j_statements_per_ms,sesame_statements_per_ms")
+    logger.info(s"Writing test results to ${file.name}")
 
-    file.appendLine(s"$threads,$rdf4jStatementsPerMs,$sesameStatementsPerMs")
+    for (threads <- minThreads to maxThreads) {
+      val rdf4jStatementsPerMs = RDFPerformanceTest.run(RDF4J, threads, warmupMillis, testLengthMillis)
+      val sesameStatementsPerMs = RDFPerformanceTest.run(Sesame, threads, warmupMillis, testLengthMillis)
 
-    logger.info(s"Writing result to ${file.name} $threads threads: RDF4J $rdf4jStatementsPerMs ms, Sesame $sesameStatementsPerMs ms")
+      file.appendLine(s"$threads,$rdf4jStatementsPerMs,$sesameStatementsPerMs")
+
+      logger.info(s"Writing result to ${file.name} $threads threads: RDF4J $rdf4jStatementsPerMs ms, Sesame $sesameStatementsPerMs ms")
+    }
+    
+    logger.info(s"Finished tests, results written to ${file.name}")
+
   }
-
 }
