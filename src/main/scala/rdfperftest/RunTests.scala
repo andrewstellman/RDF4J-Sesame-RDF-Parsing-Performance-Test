a0efd211106c1c7f88ea06e746f77121e7f59d62
Andrew Stellman <astellman@stellman-greene.com>
2017-09-26 13:54:24 -0500

    Fixed file writing

--- src/main/scala/rdfperftest/RunTests.scala
+++ src/main/scala/rdfperftest/RunTests.scala
@@ -8,6 +8,7 @@ object RunTests extends App {
   private[this] val logger = getLogger
 
   val file = File(s"test_results_${System.currentTimeMillis}.txt")
+  file.appendLine("threads,rdf4j_statements_per_ms,sesame_statements_per_ms")
   logger.info(s"Writing test results to ${file.name}")
 
   val warmupMillis = 10000
@@ -17,9 +18,8 @@ object RunTests extends App {
     val rdf4jStatementsPerMs = RDFPerformanceTest.run(RDF4J, threads, warmupMillis, testLengthMillis)
     val sesameStatementsPerMs = RDFPerformanceTest.run(Sesame, threads, warmupMillis, testLengthMillis)
 
-    file.appendLine("threads,rdf4j_statements_per_ms,sesame_statements_per_ms")
     file.appendLine(s"$threads,$rdf4jStatementsPerMs,$sesameStatementsPerMs")
-    
+
     logger.info(s"Writing result to ${file.name} $threads threads: RDF4J $rdf4jStatementsPerMs ms, Sesame $sesameStatementsPerMs ms")
   }
 
